e53d699771f2fdc0a3cb012a62c47b5f2104f5df
areeba <SP23-BCS-021@CUILAHORE.EDU.PK>
2024-02-04 14:39:56 +0500

    main

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
 
 	public static void main(String[] args) {
 
-//        Cards c = new Cards("ace", "diamonds");
-//
+        Cards c = new Cards("ace", "diamonds");
+
 		Deck d = new Deck();
-//
-//        System.out.println(d);
-//
+
+        System.out.println(d);
+
 		d.Shuffle();
-//
-//        System.out.println(d);
+
+        System.out.println(d);
 
 		Game G = new Game(d);
 
